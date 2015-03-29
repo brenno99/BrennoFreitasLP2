308f765de2be6f7b4c7b6230bedb872d98711375
BrennoFreitasLP2 <brennofreitassilva@gmail.com>
2015-03-29 18:14:19 -0300

    Exercícios em sala de aula e de casa de LP2 do dia 24/03/2015.

---
+++ Exercicio_aula240315LP2/Exercicio_aula240315LP2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_aula240315LP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double numeros;
+            double soma = 0;
+            double maior = -1;
+            double menor = 1000;
+
+            Console.WriteLine("Digite o valor de n: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite os números: ");
+                numeros = double.Parse(Console.ReadLine());
+
+                soma += numeros;
+
+                if (maior < numeros)
+                {
+                    maior = numeros;
+                        
+                }
+
+                if (menor > numeros)
+                {
+
+                    menor = numeros;
+                }
+
+            }
+
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("O menor número é: " +  menor);
+            Console.WriteLine("A soma dos números é: " + soma);
+              
+                }
+             }  
+         }
+                
+              
+             
+              
+
+                      
+            
+      
---
+++ Exercíco_VogaisLP2/Exercíco_VogaisLLP2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercíco_VogaisLLP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            int tamanho = 0;
+            int qtd_vogal = 0;
+            string letra;
+            int indice_frase = 0;
+            
+            Console.WriteLine("Digite a frase: ");
+            frase = Console.ReadLine();
+
+            tamanho = frase.Length;
+            Console.WriteLine("O tamnho da frase é: " + tamanho);
+
+            
+           
+            while (indice_frase < tamanho)
+            {
+                letra = frase.Substring(indice_frase, 1);
+                letra = letra.ToUpper();
+                
+                
+                if (letra == "A" | letra == "E" | letra == "I" | letra == "O" | letra == "U")
+                {
+                    qtd_vogal++;
+                }
+
+                indice_frase++;
+            }
+
+            Console.WriteLine("O número de vogais é: " + qtd_vogal);
+
+             
+
+        
+        
+        
+        
+        
+        }
+    }
+}
