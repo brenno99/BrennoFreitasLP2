8781a72298fc8425e1170ddb69d002aaccc56630
BrennoFreitasLP2 <brennofreitassilva@gmail.com>
2015-04-04 14:12:54 -0300

    Exercios em sala ou casa de LP2.

---
+++ Exercicios/Exercício_aula31032015/Exercício_aula31032015/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_aula31032015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int potencia = 1;
+            
+            
+            Console.WriteLine("Digite um valor para x: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de y: ");
+            y = int.Parse(Console.ReadLine());
+
+            
+            
+            for (int i = 0; i < y ; i++)
+            {
+                potencia *= x;
+            }
+
+            Console.WriteLine("A potenciação de x elevado a y é: " + potencia);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
