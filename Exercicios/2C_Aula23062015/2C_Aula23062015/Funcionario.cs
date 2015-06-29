e3eeba2b331cb3013761bd3f88c6e136805c3733
BrennoFreitasLP2 <brennofreitassilva@gmail.com>
2015-06-28 21:29:14 -0300

    Exercicios relacionados a LP2

---
+++ Exercicios/2C_Aula23062015/2C_Aula23062015/Funcionario.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2C_Aula23062015
+{
+    class Funcionario
+    {
+        public ArrayList nomes = new ArrayList();
+        public ArrayList cpf = new ArrayList();
+        public ArrayList datasNasc = new ArrayList();
+        public ArrayList datasAdmi = new ArrayList();
+        public ArrayList salarios = new ArrayList();
+
+
+        public double CalculaSalarioReajust(int j)
+        {
+            double salario_reajust = 0;
+
+            if (Convert.ToInt32(datasAdmi[j]) >= 2014)
+            {
+                salario_reajust = Convert.ToDouble(salarios[j]) + 423.45;
+            }
+
+            else if (Convert.ToInt32(datasAdmi[j]) >= 2010 && Convert.ToInt32(datasAdmi[j]) <= 2013)
+            {
+                double aumento = Convert.ToDouble(salarios[j]) * 9.84 / 100;
+                salario_reajust = Convert.ToDouble(salarios[j]) + aumento;
+            }
+
+            else if (Convert.ToInt32(datasAdmi[j]) >= 2000 && Convert.ToInt32(datasAdmi[j]) <= 2009)
+            {
+                double aumento2 = Convert.ToDouble(salarios[j]) * 16.4 / 100;
+                salario_reajust = Convert.ToDouble(salarios[j]) + aumento2;
+            }
+
+            else if (Convert.ToInt32(datasAdmi[j]) <= 1999)
+            {
+                double aumento3 = Convert.ToDouble(salarios[j]) * 22.6 / 100;
+                salario_reajust = Convert.ToDouble(salarios[j]) + aumento3;
+            }
+
+            return salario_reajust;
+        }
+        
+
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+
--- Exercicios/2C_Aula23062015/2C_Aula23062015/Program.cs
+++ Exercicios/2C_Aula23062015/2C_Aula23062015/Program.cs
@@ -11,54 +11,33 @@ namespace _2C_Aula23062015
         static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine());
-            string[] nome = new string[N];
-            int[] cpf = new int[N];
-            string[] DataNasc = new string[N];
-            double[] salario = new double[N];
-            int[] dataAdmi = new int[N];
-            double[] salario_reajust = new double[N];
-           
+            double salarioReajust = 0;
+
+            Funcionario F = new Funcionario();
+
             for (int i = 0; i < N; i++)
             {
-                nome[i] = Console.ReadLine();
-                cpf[i] = int.Parse(Console.ReadLine());
-                DataNasc[i] = Console.ReadLine();
-                salario[i] = double.Parse(Console.ReadLine());
-                dataAdmi[i] = int.Parse(Console.ReadLine());
-
-                if (dataAdmi[i] >= 2014)
-                {
-                   salario_reajust[i] = salario[i] + 423.45;
-                }
-
-                else if (dataAdmi[i] > 2010 && dataAdmi[i] < 2013)
-                {
-                    double aumento = salario[i] * 9.84 / 100;
-                    salario_reajust[i] = salario[i] + aumento;                
-                }
-
-                else if (dataAdmi[i] > 2000 && dataAdmi[i] < 2010)
-                {
-                    double aumento2 = salario[i] * 16.4 / 100;
-                    salario_reajust[i] = salario[i] + aumento2;
-                }
-
-                else if (dataAdmi[i] < 1999)
-                {
-                    double aumento3 = salario[i] * 22.6 / 100;
-                    salario_reajust[i] = salario[i] + aumento3;
-                }
+                F.nomes.Add(Console.ReadLine());
+                F.cpf.Add(Console.ReadLine());
+                F.datasNasc.Add(Console.ReadLine());
+                F.datasAdmi.Add(Console.ReadLine());
+                F.salarios.Add(Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine();
 
             }
 
-            for (int j = 0; j < N; j++)
+            for (int k = 0; k < N; k++)
             {
-                Console.WriteLine("O CPF é: " + cpf[j]);
-                Console.WriteLine("O nome é: " + nome[j]);
-                Console.WriteLine("A data de nascimento é: " + DataNasc[j]);
-                Console.WriteLine("A data de admissão é: " + dataAdmi[j]);
-                Console.WriteLine("O salário é: " + salario[j]);
-                Console.WriteLine("O salário reajustado é: " + salario_reajust[j]);
+                Console.WriteLine("Nome: " + F.nomes[k]);
+                Console.WriteLine("CPF: " + F.cpf[k]);
+                Console.WriteLine("Data de Nascimento: " + F.datasNasc[k]);
+                Console.WriteLine("Data de Admissão: " + F.datasAdmi[k]);
+                Console.WriteLine("Salário: " + F.salarios[k]);
+
+                salarioReajust = F.CalculaSalarioReajust(k);
+
+                Console.WriteLine("Novo Salário: " + salarioReajust);
+                Console.WriteLine();
             }
         }
     }
