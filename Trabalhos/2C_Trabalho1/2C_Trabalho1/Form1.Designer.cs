d2b248a11bc985c94da2414092931ff8a7131bb6
BrennoFreitasLP2 <brennofreitassilva@gmail.com>
2015-08-08 19:12:21 -0300

    Trabalhos relacionados a LP2

---
+++ Trabalhos/2C_Trabalho1/2C_Trabalho1/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace _2C_Trabalho1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.txtCapacidadeCarro = new System.Windows.Forms.TextBox();
+            this.txtidCarro = new System.Windows.Forms.TextBox();
+            this.btn_Cadastrar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtQtdCarros = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_Iniciar = new System.Windows.Forms.Button();
+            this.btn_Limpar = new System.Windows.Forms.Button();
+            this.btn_listarPassageiros = new System.Windows.Forms.Button();
+            this.btn_Solicitar_Vaga = new System.Windows.Forms.Button();
+            this.txtCarros = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Modelo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marca";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Placa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Capacidade Carro";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(189, 48);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(100, 20);
+            this.txtModelo.TabIndex = 4;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(189, 85);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(100, 20);
+            this.txtMarca.TabIndex = 5;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(189, 127);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(100, 20);
+            this.txtPlaca.TabIndex = 6;
+            // 
+            // txtCapacidadeCarro
+            // 
+            this.txtCapacidadeCarro.Location = new System.Drawing.Point(189, 169);
+            this.txtCapacidadeCarro.Name = "txtCapacidadeCarro";
+            this.txtCapacidadeCarro.Size = new System.Drawing.Size(100, 20);
+            this.txtCapacidadeCarro.TabIndex = 7;
+            // 
+            // txtidCarro
+            // 
+            this.txtidCarro.Location = new System.Drawing.Point(101, 294);
+            this.txtidCarro.Multiline = true;
+            this.txtidCarro.Name = "txtidCarro";
+            this.txtidCarro.Size = new System.Drawing.Size(136, 29);
+            this.txtidCarro.TabIndex = 8;
+            // 
+            // btn_Cadastrar
+            // 
+            this.btn_Cadastrar.Location = new System.Drawing.Point(189, 218);
+            this.btn_Cadastrar.Name = "btn_Cadastrar";
+            this.btn_Cadastrar.Size = new System.Drawing.Size(99, 23);
+            this.btn_Cadastrar.TabIndex = 9;
+            this.btn_Cadastrar.Text = "Cadastrar";
+            this.btn_Cadastrar.UseVisualStyleBackColor = true;
+            this.btn_Cadastrar.Click += new System.EventHandler(this.btn_Cadastrar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Quantidade Carros";
+            // 
+            // txtQtdCarros
+            // 
+            this.txtQtdCarros.Location = new System.Drawing.Point(189, 13);
+            this.txtQtdCarros.Name = "txtQtdCarros";
+            this.txtQtdCarros.Size = new System.Drawing.Size(100, 20);
+            this.txtQtdCarros.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "id Carro";
+            // 
+            // btn_Iniciar
+            // 
+            this.btn_Iniciar.Location = new System.Drawing.Point(323, 13);
+            this.btn_Iniciar.Name = "btn_Iniciar";
+            this.btn_Iniciar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Iniciar.TabIndex = 13;
+            this.btn_Iniciar.Text = "Iniciar";
+            this.btn_Iniciar.UseVisualStyleBackColor = true;
+            this.btn_Iniciar.Click += new System.EventHandler(this.btn_Iniciar_Click);
+            // 
+            // btn_Limpar
+            // 
+            this.btn_Limpar.Location = new System.Drawing.Point(32, 218);
+            this.btn_Limpar.Name = "btn_Limpar";
+            this.btn_Limpar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Limpar.TabIndex = 14;
+            this.btn_Limpar.Text = "Limpar";
+            this.btn_Limpar.UseVisualStyleBackColor = true;
+            this.btn_Limpar.Click += new System.EventHandler(this.btn_Limpar_Click);
+            // 
+            // btn_listarPassageiros
+            // 
+            this.btn_listarPassageiros.Location = new System.Drawing.Point(44, 355);
+            this.btn_listarPassageiros.Name = "btn_listarPassageiros";
+            this.btn_listarPassageiros.Size = new System.Drawing.Size(159, 23);
+            this.btn_listarPassageiros.TabIndex = 15;
+            this.btn_listarPassageiros.Text = "Listar Passageiros";
+            this.btn_listarPassageiros.UseVisualStyleBackColor = true;
+            this.btn_listarPassageiros.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Solicitar_Vaga
+            // 
+            this.btn_Solicitar_Vaga.Location = new System.Drawing.Point(282, 294);
+            this.btn_Solicitar_Vaga.Name = "btn_Solicitar_Vaga";
+            this.btn_Solicitar_Vaga.Size = new System.Drawing.Size(116, 23);
+            this.btn_Solicitar_Vaga.TabIndex = 16;
+            this.btn_Solicitar_Vaga.Text = "Solicitar Vaga";
+            this.btn_Solicitar_Vaga.UseVisualStyleBackColor = true;
+            this.btn_Solicitar_Vaga.Click += new System.EventHandler(this.btn_Solicitar_Vaga_Click);
+            // 
+            // txtCarros
+            // 
+            this.txtCarros.Location = new System.Drawing.Point(48, 404);
+            this.txtCarros.Multiline = true;
+            this.txtCarros.Name = "txtCarros";
+            this.txtCarros.Size = new System.Drawing.Size(358, 90);
+            this.txtCarros.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 495);
+            this.Controls.Add(this.txtCarros);
+            this.Controls.Add(this.btn_Solicitar_Vaga);
+            this.Controls.Add(this.btn_listarPassageiros);
+            this.Controls.Add(this.btn_Limpar);
+            this.Controls.Add(this.btn_Iniciar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtQtdCarros);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_Cadastrar);
+            this.Controls.Add(this.txtidCarro);
+            this.Controls.Add(this.txtCapacidadeCarro);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Viagem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.TextBox txtCapacidadeCarro;
+        private System.Windows.Forms.TextBox txtidCarro;
+        private System.Windows.Forms.Button btn_Cadastrar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtQtdCarros;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_Iniciar;
+        private System.Windows.Forms.Button btn_Limpar;
+        private System.Windows.Forms.Button btn_listarPassageiros;
+        private System.Windows.Forms.Button btn_Solicitar_Vaga;
+        private System.Windows.Forms.TextBox txtCarros;
+    }
+}
+
---
+++ Trabalhos/2C_Trabalho1/2C_Trabalho1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;  
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2C_Trabalho1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        //ArrayList carros = new ArrayList();
+        List<Carro> carros = new List<Carro>();
+
+        public int qtdCarrosInformada;
+        public int qtdCarros = 0 ; 
+
+        private void btn_Cadastrar_Click(object sender, EventArgs e)
+        {
+                qtdCarros = qtdCarros + 1;
+                if (qtdCarros <= qtdCarrosInformada)
+                {
+                    Carro c = new Carro();
+                    c.Set_idCarro(qtdCarros);
+                    c.Set_modelo(txtModelo.Text);
+                    c.Set_marca(txtMarca.Text);
+                    c.Set_placa(txtPlaca.Text);
+                    c.Set_capacCarro(int.Parse(txtCapacidadeCarro.Text));
+                    c.Set_qtdPassag(0);
+                    carros.Add(c);
+
+                    Limpar_Campos();
+                }
+                else
+                {
+                    MessageBox.Show("Todos os carros já foram cadastrados!");
+                    Limpar_Campos();
+                }
+
+                       
+        }
+
+        private void btn_Iniciar_Click(object sender, EventArgs e)
+        {
+            qtdCarrosInformada = int.Parse(txtQtdCarros.Text);
+            txtModelo.Focus();
+        }
+
+        private void btn_Limpar_Click(object sender, EventArgs e)
+        {
+            Limpar_Campos();
+        }
+
+
+        private void Limpar_Campos()
+        {
+            txtModelo.Clear();
+            txtMarca.Clear();
+            txtPlaca.Clear();
+            txtCapacidadeCarro.Clear();
+            txtModelo.Focus();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int total_passag = 0;
+            int qtd_vagas_disponiveis = 0;
+
+            for (int k = 0; k < carros.Count; k++)
+            {
+                Carro c2 = new Carro();
+                c2 = (Carro)carros[k];
+
+                total_passag = total_passag + carros[k].Get_qtdPassag();
+
+                qtd_vagas_disponiveis = carros[k].Get_capacCarro() - carros[k].Get_qtdPassag();
+                txtCarros.AppendText("Carro " + (k + 1) + "\r\n" + "Modelo: " + carros[k].Get_modelo() + " - " + "Marca: " + carros[k].Get_marca() + " " + "Placa: " + carros[k].Get_placa() + "\r\n" + "\r\n" + "Quantidade de vagas disponíveis: " + qtd_vagas_disponiveis + "\r\n" + "\r\n");
+
+                if (carros[k].Get_qtdPassag() >= carros[k].Get_capacCarro())
+                {
+                    txtCarros.AppendText("Carro lotado!");
+                }
+               
+            } // for 
+           
+            txtCarros.AppendText("Total de pessoas na viagem: " + total_passag);
+        } // listar passageiros
+                     
+
+        private void btn_Solicitar_Vaga_Click(object sender, EventArgs e)
+        {
+
+            int id_carro = int.Parse(txtidCarro.Text); 
+            string existe_carro = "Não";
+               
+           // Pesquisar o id_carro no array de objetos 
+                
+           for (int k = 0; k < carros.Count; k++)
+                {
+                    Carro c1 = new Carro();
+                    c1 = (Carro)carros[k];                                  
+                                       
+                    if (c1.Get_idCarro() == id_carro)
+                    {
+                        existe_carro = "Sim";
+                       
+                        if (c1.Get_qtdPassag() >= c1.Get_capacCarro())
+                        {
+                            MessageBox.Show("O carro " + id_carro.ToString() +  " está lotado! " );
+                        }
+                        
+                        else
+                        {
+                            c1.Set_qtdPassag(c1.Get_qtdPassag() + 1);
+                            carros[k] = c1;
+                            MessageBox.Show("Solicitação efetuada!");
+                        }
+                                                
+                    }// get_idCarro == id_carro 
+
+           }// for carros.Count
+
+          if (existe_carro == "Não")
+          {
+             MessageBox.Show("Não existe o carro!");
+          }
+
+          txtidCarro.Clear(); 
+
+        } // solicitar vaga 
+
+
+
+        }
+    }
+
